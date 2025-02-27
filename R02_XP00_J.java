0e9528f9f4efe349cec26f86b791965736ddcb9c
Brandon Kates <kates.brandon01@gmail.com>
2025-02-26 19:36:12 -0500

    second rule

---
+++ R02_XP00_J.java
@@ -0,0 +1,17 @@
+import java.io.File;
+/*
+Rule 02. Expressions (EXP)
+XP00-J. Do not ignore values returned by methods Given the non-compliant code
+below:
+*/
+public class R02_XP00_J{
+    public static void main(String[] args) {
+
+    }
+    public void deleteFile(){
+        File someFile = new File("someFileName.txt");
+        if (!someFile.delete()){
+            // Handle failure to delete the file
+        }
+    }
+}
